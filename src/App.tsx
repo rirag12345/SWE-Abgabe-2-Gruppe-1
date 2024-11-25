c11072d2193533f913cdee2894178e4c00782029
Philip Neuffer <neph1015@h-ka.de>
2024-11-25 21:17:06 +0100

    Add EditorConfig and App.css, refactor App component for improved styling and readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() { 
-  const [data] = useState([]) 
+function App() {
+  const [data] = useState([])
   useEffect(() => {
     const fetchBook = async () => {
         const results = await axios.get("https://localhost:3000/rest")
@@ -14,8 +15,12 @@ function App() {
 
   return (
     <div>
-      {/* Die Daten werden als JSON-String im div-Element angezeigt */}
-      {JSON.stringify(data)}
+      <div>
+        {/* Die Daten werden als JSON-String im div-Element angezeigt */}
+        {JSON.stringify(data)}
+      </div>
+      {/*display Vite logo*/}
+      <img src="/vite.svg" alt="Vite logo" className="Vite-logo-class" />
     </div>
-  )
+  );
 }
