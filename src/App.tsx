352d8478e2c6137b688d45f1f92f81f45a07b299
Philip Neuffer <neph1015@h-ka.de>
2024-11-25 21:17:06 +0100

    Add EditorConfig and App.css, refactor App component for improved styling and readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 
-export function App() { 
+export function App() {
   /*
   Initialer Wert für den State, der mit useState verwaltet wird
   => Array Destructuring
   */
-  const [data, setData] = useState([]) 
+  const [data, setData] = useState([]);
 
-  useEffect(() => { 
-    // useEffect wird aufgerufen, wenn die Abhängigkeiten sich ändern. 
+  useEffect(() => {
+    // useEffect wird aufgerufen, wenn die Abhängigkeiten sich ändern.
     // Da das Abhängigkeitsarray leer ist, wird es nur einmal beim Laden der Komponente ausgeführt.
-    axios.get("http://localhost:3000/buch") 
+    axios
+      .get("http://localhost:3000/buch")
       // Sendet einen GET-Request an die angegebene URL und speichert die Antwort in der Variable response
-      .then(response => setData(response.data)) 
-      // Setzt den State 'data' mit den erhaltenen Daten
-  }, []) 
+      .then((response) => setData(response.data));
+    // Setzt den State 'data' mit den erhaltenen Daten
+  }, []);
 
   return (
     <div>
-      {/* Die Daten werden als JSON-String im div-Element angezeigt */}
-      {JSON.stringify(data)}
+      <div>
+        {/* Die Daten werden als JSON-String im div-Element angezeigt */}
+        {JSON.stringify(data)}
+      </div>
+      {/*display Vite logo*/}
+      <img src="/vite.svg" alt="Vite logo" className="Vite-logo-class" />
     </div>
-  )
+  );
 }
