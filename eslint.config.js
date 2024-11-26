5e01a2ba496e6033ffce9b71bc157f9dbace5e8e
Philip Neuffer <neph1015@h-ka.de>
2024-11-26 09:20:48 +0100

    Add React plugin to ESLint configuration and update parser options

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
 
 export default tseslint.config(
   { ignores: ["dist"] },
