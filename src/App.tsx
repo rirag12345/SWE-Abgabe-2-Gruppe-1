7966c65ecfbb7a203bee85cfdf0571355ab57db4
Botan Coban <146077067+BtnCbn@users.noreply.github.com>
2024-11-26 16:00:26 +0100

    App.tsx Änderung

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,15 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 
 function App() { 
-  // Initialer Wert für den State, der mit useState verwaltet wird
-  const [data, setData] = useState([]) 
+  const [data] = useState([]) 
+  useEffect(() => {
+    const fetchBook = async () => {
+        const results = await axios.get("https://localhost:3000/rest")
+        console.log(results)
+      };
 
-  useEffect(() => { 
-    // useEffect wird aufgerufen, wenn die Abhängigkeiten sich ändern. 
-    // Da das Abhängigkeitsarray leer ist, wird es nur einmal beim Laden der Komponente ausgeführt.
-    axios.get("http://localhost:3000/buch") 
-      // Sendet einen GET-Request an die angegebene URL und speichert die Antwort in der Variable response
-      .then(response => setData(response.data)) 
-      // Setzt den State 'data' mit den erhaltenen Daten
-  }, []) 
+    void fetchBook();
+  }, []);
 
   return (
     <div>
