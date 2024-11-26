2a91d260b803d1500da4c1dff395be9b709588a8
Philip Neuffer <neph1015@h-ka.de>
2024-11-26 10:19:27 +0100

    Refactor ESLint configuration and update dependencies for consistency

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,9 @@
-import js from "@eslint/js";
 import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import react from "eslint-plugin-react";
+import pluginReact from "eslint-plugin-react";
 
 export default tseslint.config(
   { ignores: ["dist"] },
