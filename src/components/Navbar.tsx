b8d4bb78a644a6185c2a841eb825036b1acef906
Botan Coban <146077067+BtnCbn@users.noreply.github.com>
2024-11-28 20:49:26 +0100

    Added Navbar Prototype in components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,1 +1,77 @@
 
+import { AppBar, Toolbar, Typography, Button, InputBase } from '@mui/material';
+import { styled, alpha } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
+import { Link } from 'react-router-dom';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: theme.spacing(2),
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+const Navbar = () => {
+  return (
+    <AppBar
+      position="static"
+      style={{ backgroundColor: '#f5f5f5', color: '#000', boxShadow: 'none' }}
+    >
+      <Toolbar>
+        {/* Titel */}
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          Buch Projekt
+        </Typography>
+
+        {/* MUI Buttons */}
+        <Button color="inherit" component={Link} to="/signin">Sign In</Button>
+        <Button color="inherit" component={Link} to="/">Home</Button>
+        <Button color="inherit" component={Link} to="/about">About</Button>
+        <Button color="inherit" component={Link} to="/contact">Contact</Button>
+
+        {/* Searchbar */}
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Suchen…"
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </Search>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
