9372fa0c6c3b546b868225186749c3272949dd6c
Botan Coban <146077067+BtnCbn@users.noreply.github.com>
2024-11-28 20:43:02 +0100

    Added home directory

---
+++ src/pages/home.tsx
@@ -0,0 +1,16 @@
+import { Container, Typography } from '@mui/material';
+
+const Home = () => {
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Home Page
+      </Typography>
+      <Typography>
+        Willkommen auf der Startseite!
+      </Typography>
+    </Container>
+  );
+};
+
+export default Home;
