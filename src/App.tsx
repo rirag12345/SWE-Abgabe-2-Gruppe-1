0d179dd06aad27695a7d82b59c9594b1258f3fd0
Botan Coban <146077067+BtnCbn@users.noreply.github.com>
2024-12-10 21:57:17 +0100

    Replaced useEFFECT to another file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Container } from '@mui/material';
 import SignIn from './components/SignIn';
@@ -7,17 +5,6 @@ import Navbar from './components/Navbar';
 import SearchResults from './pages/searchResults';
 
 function App() {
-  const [data] = useState([]);
-
-  useEffect(() => {
-    const fetchBook = async () => {
-      const results = await axios.get("https://localhost:3000/rest");
-      console.log(results);
-    };
-
-    void fetchBook();
-  }, []);
-
   return (
     <Router>
       <div style={{ backgroundColor: '#fff', height: '100vh', margin: 0 }}>
@@ -32,9 +19,6 @@ function App() {
             <Route path="/search-results" element={<SearchResults />} />
             {/* Weitere Routen hier */}
           </Routes>
-
-          {/* JSON-Daten */}
-          <div>{JSON.stringify(data)}</div>
         </Container>
       </div>
     </Router>
