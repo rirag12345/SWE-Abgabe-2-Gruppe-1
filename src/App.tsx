9d56e50583829ff74825c6510e1b7489dbea36b3
Botan Coban <146077067+BtnCbn@users.noreply.github.com>
2024-12-12 15:46:41 +0100

    Changed import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Container } from '@mui/material';
 import SignIn from './components/SignIn';
 import Navbar from './components/Navbar';
-import SearchResults from './pages/searchResults';
+import SearchResults from './pages/searchresults';
 
 function App() {
   return (
