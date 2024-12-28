17bdb1e030f6e9904bbe393277cd8d477c310790
Philip Neuffer <neph1015@h-ka.de>
2024-12-28 19:20:32 +0100

    fix: remove unused state variables and implement localStorage for JWT and refreshToken in SignIn component

--- src/components/Sign-in.tsx
+++ src/components/Sign-in.tsx
@@ -10,17 +10,9 @@ const tokenUrl: string = import.meta.env.VITE_TOKEN_URL as string;
 export const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [JWT, setJWT] = useState('');
-  const [refreshToken, setRefreshToken] = useState('');
   const [emailError, setEmailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
-
   const navigate: NavigateFunction = useNavigate();
-
-  //TODO useEffect und lokalStorage
-  /*this is needed due to the asynchronous nature of updating stateful react variables
-    otherwise the post request could occasionally be run before the 'setEmailError' and 'setPasswordError' functions finish executing
-  */
 
   useEffect(() => {
     try {
@@ -53,8 +45,8 @@ export const SignIn = () => {
         password: password,
       })
       .then((result) => {
-        console.log('JWT:\n', result.data.access_token);
-        console.log('refresh:\n', result.data.refresh_token);
+        localStorage.setItem('JWT', result.data.access_token);
+        localStorage.setItem('refreshToken', result.data.refresh_token);
 
         //Reset email and password so they are not exposed longer then needed
         setEmail('');
