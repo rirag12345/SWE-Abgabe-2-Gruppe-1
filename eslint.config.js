3e70ca0e51baedc3a142721947338310d9cbbd65
Felix Jaeger <152805046+felix4132@users.noreply.github.com>
2025-01-04 17:11:06 +0100

    fix: "npm run lint" causing error
    added missing import (line 1) and refactor

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,43 @@
-import globals from "globals";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import pluginJs from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ["dist"] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
-        tsconfigRootDir: import.meta.dirname,
-        ...tseslint.configs.stylisticTypeChecked,
-      },
+    { ignores: ['dist', 'node_modules'] },
+    {
+        extends: [
+            pluginJs.configs.recommended,
+            ...tseslint.configs.recommended,
+        ],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                project: ['./tsconfig.node.json', './tsconfig.app.json'],
+                tsconfigRootDir: import.meta.dirname,
+                ...tseslint.configs.stylisticTypeChecked,
+            },
+        },
+        // Set the react version
+        settings: { react: { version: '18.3' } },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            react,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            // Enable recommended rules for react
+            ...react.configs.recommended.rules,
+            ...react.configs['jsx-runtime'].rules,
+        },
     },
-    // Set the react version
-    settings: { react: { version: "18.3" } },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      react,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      // Enable recommended rules for react
-      ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,
-    },
-  }
 );
