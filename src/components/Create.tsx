e1d3ace30f99b358095f3daa77656bad8dc18195
Felix Jaeger <152805046+felix4132@users.noreply.github.com>
2025-01-06 01:31:40 +0100

    feat: implemented form with submission for better UX

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -9,17 +9,10 @@ export function Create() {
     const [preis, setPreis] = useState('');
     const [rabatt, setRabatt] = useState('');
     const [titel, setTitel] = useState('');
-    // const [art, setArt] = useState('');
-    // const [lieferbar, setLieferbar] = useState('');
-    // const [datum, setDatum] = useState('');
-    // const [homepage, setHomepage] = useState('');
-    // const [schlagwoerter, setSchlagwoerter] = useState('');
-    // const [untertitel, setUntertitel] = useState('');
-    // const [abbildungen, setAbbildungen] = useState('');
     const [buttonDisabled, setButtonDisabled] = useState(false);
     const [showAlert, setShowAlert] = useState(false);
 
-    async function handleClick() {
+    async function handleSubmit() {
         setButtonDisabled(true);
         try {
             const response = await axios.post(
@@ -28,7 +21,7 @@ export function Create() {
                     isbn: isbn,
                     rating: parseInt(rating),
                     preis: parseFloat(preis),
-                    rabatt: setRabatt(rabatt),
+                    rabatt: parseFloat(rabatt),
                     titel: { titel: titel },
                 },
                 {
@@ -59,6 +52,12 @@ export function Create() {
     return (
         <Container>
             <Box
+                component='form'
+                autoComplete='off'
+                onSubmit={(event) => {
+                    event.preventDefault(); // preventing default behavior of reloading the whole page after a form submission
+                    handleSubmit();
+                }}
                 sx={{
                     m: 'auto',
                     display: 'flex',
@@ -108,57 +107,8 @@ export function Create() {
                     value={titel}
                     onChange={(e) => setTitel(e.target.value)}
                 />
-                {/* <TextField
-                    id='art-textField'
-                    label='art'
-                    variant='filled'
-                    value={art}
-                    onChange={(e) => setArt(e.target.value)}
-                />
-                <TextField
-                    id='lieferbar-textField'
-                    label='lieferbar'
-                    variant='filled'
-                    value={lieferbar}
-                    onChange={(e) => setLieferbar(e.target.value)}
-                />
-                <TextField
-                    id='datum-textField'
-                    label='datum'
-                    variant='filled'
-                    value={datum}
-                    onChange={(e) => setDatum(e.target.value)}
-                />
-                <TextField
-                    id='homepage-textField'
-                    label='homepage'
-                    variant='filled'
-                    value={homepage}
-                    onChange={(e) => setHomepage(e.target.value)}
-                />
-                <TextField
-                    id='schlagwoerter-textField'
-                    label='schlagwoerter'
-                    variant='filled'
-                    value={schlagwoerter}
-                    onChange={(e) => setSchlagwoerter(e.target.value)}
-                />
-                <TextField
-                    id='untertitel-textField'
-                    label='untertitel'
-                    variant='filled'
-                    value={untertitel}
-                    onChange={(e) => setUntertitel(e.target.value)}
-                />
-                <TextField
-                    id='abbildungen-textField'
-                    label='abbildungen'
-                    variant='filled'
-                    value={abbildungen}
-                    onChange={(e) => setAbbildungen(e.target.value)}
-                /> */}
                 <Button
-                    onClick={handleClick}
+                    type='submit'
                     variant='contained'
                     endIcon={<SendIcon />}
                     disabled={buttonDisabled}
