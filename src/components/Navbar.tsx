cc1471bb1ccb2b5c0b9046f40e8cc5c7ca9be732
Botan Coban <146077067+BtnCbn@users.noreply.github.com>
2025-01-07 18:22:09 +0100

    feat: add component link to icon instead of home button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,48 +1,43 @@
 import AutoStoriesIcon from '@mui/icons-material/AutoStories';
-import { AppBar, Box, Button, Toolbar } from '@mui/material';
+import { AppBar, Box, Button, Toolbar, IconButton } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
-    return (
-        <AppBar
-            position="static"
-            style={{
-                backgroundColor: '#f5f5f5',
-                color: '#000',
-                boxShadow: 'none',
-            }}
+  return (
+    <AppBar
+      position="static"
+      style={{
+        backgroundColor: '#f5f5f5',
+        color: '#000',
+        boxShadow: 'none',
+      }}
+    >
+      <Toolbar>
+        <IconButton component={Link} to="/" color="inherit">
+          <AutoStoriesIcon />
+        </IconButton>
+        <Box sx={{ flexGrow: 1 }}>
+          <Button color="inherit" component={Link} to="/search-results">
+            Search
+          </Button>
+          <Button color="inherit" component={Link} to="/create">
+            Create
+          </Button>
+        </Box>
+        <Box
+          sx={{
+            border: '1px solid #000',
+            borderRadius: '4px',
+            padding: '4px 8px',
+            backgroundColor: '#fff',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+          }}
         >
-            <Toolbar>
-                <AutoStoriesIcon />
-                <Box sx={{ flexGrow: 1 }}>
-                    <Button color="inherit" component={Link} to="/">
-                        Home
-                    </Button>
-                    <Button
-                        color="inherit"
-                        component={Link}
-                        to="/search-results"
-                    >
-                        Search
-                    </Button>
-                    <Button color="inherit" component={Link} to="/create">
-                        Create
-                    </Button>
-                </Box>
-                <Box
-                    sx={{
-                        border: '1px solid #000',
-                        borderRadius: '4px',
-                        padding: '4px 8p6x',
-                        backgroundColor: '#fff',
-                        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-                    }}
-                >
-                    <Button color="inherit" component={Link} to="/signin">
-                        Sign In
-                    </Button>
-                </Box>
-            </Toolbar>
-        </AppBar>
-    );
+          <Button color="inherit" component={Link} to="/signin">
+            Sign In
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 };
